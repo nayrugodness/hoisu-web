023206b45e9ac98ef7aebde8a30770d6094fbee7
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 10:55:45 +0200

    creating form ItemMneuForm

--- restaurante/forms.py
+++ restaurante/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from .models import ItemMenu, Gallery, Event, Restaurant, Users, Reservation
 from django.db.models import fields
+
+class ItemMenuForm(forms.ModelForm):
+
+    class Meta:
+        model = ItemMenu
+        fields = '__all__'
