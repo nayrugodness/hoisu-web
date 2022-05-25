d2d80702739d60bfd62aded82aee2cddb4c92225
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:00:56 +0200

    adding all models into admin.site.register

--- restaurante/admin.py
+++ restaurante/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
 # Register your models here.
+
+admin.site.register(ItemMenu)
+admin.site.register(Gallery)
+admin.site.register(Event)
+admin.site.register(Restaurant)
+admin.site.register(Reservation)
