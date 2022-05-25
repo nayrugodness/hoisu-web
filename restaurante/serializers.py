e1ba0c896dc734742b40178fc3ab3497e537d94c
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:09:41 +0200

    creating reservation Serializer

--- restaurante/serializers.py
+++ restaurante/serializers.py
@@ -21,3 +21,8 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
