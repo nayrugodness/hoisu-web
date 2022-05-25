8ce53f497770ec428c8bd2c0c421b5ba1c350585
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:48:01 +0200

    importing from rest-framework package viewsets inside  views.py

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
 from .forms import  ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
+from rest_framework import viewsets
