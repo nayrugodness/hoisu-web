8631aa0a700aa4aee5a47de06702384e5424aeb6
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:07:55 +0200

    creating ItemMenu Serializer

--- restaurante/serializers.py
+++ restaurante/serializers.py
@@ -1,3 +1,8 @@
 from django.db.models.query import QuerySet
 from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
 from rest_framework import fields, serializers
+
+class ItemMenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemMenu
+        fields = '__all__'
