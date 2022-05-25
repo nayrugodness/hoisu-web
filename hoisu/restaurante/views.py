bfe845d99de147cd3e3a01a167b1b108e8589f1b
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:47:19 +0200

    importing all forms inside  views.py

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -1,2 +1,3 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
+from .forms import  ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
