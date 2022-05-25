5abb73a140a8a5e8efbae42bdd24aaef9637dfb8
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:09:10 +0200

    creating restaurant Serializer

--- restaurante/serializers.py
+++ restaurante/serializers.py
@@ -16,3 +16,8 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
