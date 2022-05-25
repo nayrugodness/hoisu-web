1d54bc6a27dc943b0487840d95a365179550060b
Nayru <nanualexandraramirez@gmail.com>
2022-05-25 16:41:13 -0500

    adding pwa urls

--- hoisu/hoisu/urls.py
+++ hoisu/hoisu/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include('restaurante.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('pwa.urls')),
 ]
 
 if settings.DEBUG:
