256e8c0ec09185a5ebb429ea95556b5fe66b85d2
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-25 11:50:22 +0200

    importing from django.views.generic ListView, createView and Detail view, and from edit importing FormView

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -3,3 +3,5 @@ from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
 from .forms import  ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
 from rest_framework import viewsets
 from .serializers import ItemMenuSerializer, GallerySerializer, EventSerializer, RestaurantSerializer, ReservationSerializer
+from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic.edit import FormView
