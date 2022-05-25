bdd9a76c7db06218bc57534d549d5fd1527cb423
Nayru <nanualexandraramirez@gmail.com>
2022-05-25 15:40:27 -0500

    importing static

--- hoisu/hoisu/urls.py
+++ hoisu/hoisu/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('restaurante.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
 
