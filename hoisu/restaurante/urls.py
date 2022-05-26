48f397c9ef8f9f1922a0f3519e94fed7da193d5f
Nayru <nanualexandraramirez@gmail.com>
2022-05-26 18:41:27 -0500

    adding reservation update url

--- hoisu/restaurante/urls.py
+++ hoisu/restaurante/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 urlpatterns = [
     path('registrar/', views.create_restaurant, name='registrar'),
     path('modificar-restaurante/<slug:slug>', views.update_restaurant, name='modificar-restaurante'),
+    path('modificar-reservacion/<slug:slug>', views.update_restaurant, name='modificar-reservación'),
     path('reservar/', views.create_reservation, name='reservar'),
     path('listar/', views.list_restaurants, name='listar'),
 ]
