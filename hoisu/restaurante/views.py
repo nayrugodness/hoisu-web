d5b7e2dc53855e92c38d8e1bb4f8ea25f053431d
Nayru <nanualexandraramirez@gmail.com>
2022-05-26 15:14:23 -0500

    creating restaurant_create form

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -6,3 +6,15 @@ from .serializers import ItemMenuSerializer, GallerySerializer, EventSerializer,
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.views.generic.edit import FormView
 
+def restaurant_create(request):
+    if request.method == "POST":
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+    else:
+        form = RestaurantForm()
+    return render(request,'app/restaurant/create.html',{'form':form})
