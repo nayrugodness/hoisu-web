b40197c32299c111fda6a4d1a9724ad881fdc650
Nayru <nanualexandraramirez@gmail.com>
2022-05-26 15:12:23 -0500

    deleting hoidu url patterns

--- hoisu/restaurante/urls.py
+++ hoisu/restaurante/urls.py
@@ -3,8 +3,5 @@ from . import views
 from rest_framework import routers
 
 urlpatterns = [
-    path('registrar-restaurante/', views.RestaurantCreateView.as_view(), name='registrar-restaurante'),
-    path('reservar', views.ReservationCreateView.as_view(), name='reservar'),
-    path('modificar-restaurante', views.RestaurantUpdateView.as_view(), name='modificar-restaurante'),
-    path('success', views.success, name='success'),
+
 ]
