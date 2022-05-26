384606c0816abf167fa7532b9ad7dacd1176b2b2
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-26 07:17:07 +0200

    creating reservation create inside urls.py

--- hoisu/restaurante/urls.py
+++ hoisu/restaurante/urls.py
@@ -4,5 +4,6 @@ from rest_framework import routers
 
 urlpatterns = [
     path('registrar-restaurante/', views.RestaurantCreateView.as_view(), name='registrar-restaurante'),
+    path('reservar', views.ReservationCreateView.as_view(), name='reservar'),
     path('success', views.success, name='success'),
 ]
