b62cde73c48f372b5fabfe0b4f843d437d183045
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-26 06:49:55 +0200

    creating and adding create restaurant form

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
-from .forms import  ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
+from .forms import ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
 from rest_framework import viewsets
 from .serializers import ItemMenuSerializer, GallerySerializer, EventSerializer, RestaurantSerializer, ReservationSerializer
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.views.generic.edit import FormView
 
-class ReservationFormView(FormView):
+class RestaurantFormView(FormView):
     template_name = 'app/restaurant/create.html'
-    form_class = ReservationForm
+    form_class = RestaurantForm
     success_url = 'success'
 
     def form_valid(self, form):
