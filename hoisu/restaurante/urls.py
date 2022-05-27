924111691d4b8da85d7d7b3b5c741e8b5d8d2af1
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-27 06:44:55 +0200

    creating restaurant detail view url

--- hoisu/restaurante/urls.py
+++ hoisu/restaurante/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registrar/', views.create_restaurant, name='registrar'),
     path('modificar-restaurante/<slug:slug>', views.update_restaurant, name='modificar-restaurante'),
+    path('restaurante/<slug:slug>', views.RestaurantView.as_view(), name='detalle'),
     path('modificar-reservacion/<slug:slug>', views.update_reservation, name='modificar-reservación'),
     path('reservar/', views.create_reservation, name='reservar'),
     path('listar/', views.list_restaurants, name='listar'),
