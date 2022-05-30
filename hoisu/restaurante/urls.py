9a763e26f05e88b0ab17deed8f43f773e575c82d
Nayru <nanualexandraramirez@gmail.com>
2022-05-30 18:36:15 -0500

    adding urls

--- hoisu/restaurante/urls.py
+++ hoisu/restaurante/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('reservar/<slug:slug>', views.create_reservation, name='reservar'),
     path('listar/', views.list_restaurants, name='listar'),
     path('listar-reservaciones/<id>', views.list_reservations, name='reservaciones'),
+    path('accounts/login/', LoginView.as_view('template_name':'registration/login.html','form_class':LoginForm}),
+    path('login/', LoginView.as_view('template_name':'registration/login.html','form_class':LoginForm}),
 ]
