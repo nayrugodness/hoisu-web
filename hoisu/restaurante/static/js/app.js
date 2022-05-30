43744c131a69f9dd7666278787a84feb4cb3cc59
Nayru <nanualexandraramirez@gmail.com>
2022-05-29 21:23:46 -0500

    solving restaurant list style

--- hoisu/restaurante/static/js/app.js
+++ hoisu/restaurante/static/js/app.js
@@ -1,98 +1,16 @@
-/**
- * @description Change Home page slider's arrows active status
- */
-function updateSliderArrowsStatus(
-  cardsContainer,
-  containerWidth,
-  cardCount,
-  cardWidth
-) {
-  if (
-    $(cardsContainer).scrollLeft() + containerWidth <
-    cardCount * cardWidth + 15
-  ) {
-    $("#slide-right-container").addClass("active");
-  } else {
-    $("#slide-right-container").removeClass("active");
-  }
-  if ($(cardsContainer).scrollLeft() > 0) {
-    $("#slide-left-container").addClass("active");
-  } else {
-    $("#slide-left-container").removeClass("active");
-  }
-}
-$(function() {
-  // Scroll products' slider left/right
-  let div = $("#cards-container");
-  let cardCount = $(div)
-    .find(".cards")
-    .children(".card").length;
-  let speed = 1000;
-  let containerWidth = $(".container").width();
-  let cardWidth = 250;
+$('.carousel[data-type="multi"] .item').each(function() {
+	let next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
 
-  updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+	for (let i = 0; i < 2; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
 
-  //Remove scrollbars
-  $("#slide-right-container").click(function(e) {
-    if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
-      $(div).animate(
-        {
-          scrollLeft: $(div).scrollLeft() + cardWidth
-        },
-        {
-          duration: speed,
-          complete: function() {
-            setTimeout(
-              updateSliderArrowsStatus(
-                div,
-                containerWidth,
-                cardCount,
-                cardWidth
-              ),
-              1005
-            );
-          }
-        }
-      );
-    }
-    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-  });
-  $("#slide-left-container").click(function(e) {
-    if ($(div).scrollLeft() + containerWidth > containerWidth) {
-      $(div).animate(
-        {
-          scrollLeft: "-=" + cardWidth
-        },
-        {
-          duration: speed,
-          complete: function() {
-            setTimeout(
-              updateSliderArrowsStatus(
-                div,
-                containerWidth,
-                cardCount,
-                cardWidth
-              ),
-              1005
-            );
-          }
-        }
-      );
-    }
-    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-  });
-
-  // If resize action ocurred then update the container width value
-  $(window).resize(function() {
-    try {
-      containerWidth = $("#cards-container").width();
-      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-    } catch (error) {
-      console.log(
-        `Error occured while trying to get updated slider container width:
-            ${error}`
-      );
-    }
-  });
+		next.children(':first-child').clone().appendTo($(this));
+	}
 });
