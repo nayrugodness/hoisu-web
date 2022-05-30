f62eee2b70581d781d221d13a0e56a0001f0de63
Nayru <nanualexandraramirez@gmail.com>
2022-05-30 18:32:46 -0500

    creating login form in forms.py

--- hoisu/restaurante/forms.py
+++ hoisu/restaurante/forms.py
@@ -40,3 +40,10 @@ class ReservationForm(forms.ModelForm):
         widgets = {
             'reservation': forms.SelectDateWidget()
         }
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
