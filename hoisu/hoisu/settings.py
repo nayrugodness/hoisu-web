b969b30c45472e59090f1625c7b5f371831b374a
Nayru <nanualexandraramirez@gmail.com>
2022-05-30 13:53:10 -0500

    solving errors

--- hoisu/hoisu/settings.py
+++ hoisu/hoisu/settings.py
@@ -84,14 +84,11 @@ WSGI_APPLICATION = 'hoisu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'test123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
