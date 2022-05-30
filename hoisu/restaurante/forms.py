136b05edab9eae3775240d91943e009d674c8a7a
Nayru <nanualexandraramirez@gmail.com>
2022-05-30 18:49:10 -0500

    extending user creation form

--- hoisu/restaurante/forms.py
+++ hoisu/restaurante/forms.py
@@ -45,9 +45,39 @@ class ReservationForm(forms.ModelForm):
             'reservation': forms.SelectDateWidget()
         }
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-
-    def __init__(self, *args, **kwargs):
-        super(LoginForm, self).__init__(*args, **kwargs)
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=100,
+        required=True,
+        help_text='Enter Email Address',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+    )
+    first_name = forms.CharField(
+        max_length=100,
+        required=True,
+        help_text='Enter First Name',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required=True,
+        help_text='Enter Last Name',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+    )
+    username = forms.CharField(
+        max_length=200,
+        required=True,
+        help_text='Enter Username',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+    )
+    password1 = forms.CharField(
+        help_text='Enter Password',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+    )
+    password2 = forms.CharField(
+        required=True,
+        help_text='Enter Password Again',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}),
+    )
+    check = forms.BooleanField(required=True)
