523c8c2fbbda9f03d9a61ca8cfa0116db9b42e0c
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-31 10:43:21 +0200

    deleting item menu, gallery and events from views.py

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
-from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
-from .forms import ItemMenuForm, GalleryForm, EventForm, RestaurantForm, ReservationForm
+from .models import Restaurant, Reservation
+from .forms import  GalleryForm, EventForm, RestaurantForm, ReservationForm
 from rest_framework import viewsets
-from .serializers import ItemMenuSerializer, GallerySerializer, EventSerializer, RestaurantSerializer, \
+from .serializers import  RestaurantSerializer, \
     ReservationSerializer
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.views.generic.edit import FormView
