e684aa88e8e21ff869315d22d938134673eaf2c4
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-31 10:39:16 +0200

    deleting event and gallery in admin.py

--- hoisu/restaurante/admin.py
+++ hoisu/restaurante/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Gallery, Event, Restaurant, Reservation
+from .models import Restaurant, Reservation
 # Register your models here.
 
 
-admin.site.register(Gallery)
-admin.site.register(Event)
+
 admin.site.register(Restaurant)
 admin.site.register(Reservation)
