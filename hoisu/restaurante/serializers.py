911e9d83f363c2914e8dac7fb3803d9222713f8e
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-31 10:40:54 +0200

    deleting item menu, gallery and event from serializers.py

--- hoisu/restaurante/serializers.py
+++ hoisu/restaurante/serializers.py
@@ -1,21 +1,8 @@
 from django.db.models.query import QuerySet
-from .models import ItemMenu, Gallery, Event, Restaurant, Reservation
+from .models import  Restaurant, Reservation
 from rest_framework import fields, serializers
 
-class ItemMenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ItemMenu
-        fields = '__all__'
 
-class GallerySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Gallery
-        fields = '__all__'
-
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = '__all__'
 
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
