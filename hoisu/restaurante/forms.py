d9f02edd3f6a6090bb2ed79701c120df403d198a
Nayru <nanualexandraramirez@gmail.com>
2022-05-31 15:17:48 -0500

    adding userLoginForm

--- hoisu/restaurante/forms.py
+++ hoisu/restaurante/forms.py
@@ -8,6 +8,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'hi',
+        }
+))
 
 
 class RestaurantForm(forms.ModelForm):
