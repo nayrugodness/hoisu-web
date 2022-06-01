af6a2a2e7cdc4e7f282e660d6971086c7506b4cd
Nayru <nanualexandraramirez@gmail.com>
2022-05-31 20:14:21 -0500

    go to first edition

--- hoisu/restaurante/views.py
+++ hoisu/restaurante/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import authenticate, login
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import Restaurant, Reservation
 from .forms import RestaurantForm, ReservationForm, CustomUserCreationForm
 from rest_framework import viewsets
-from .serializers import  RestaurantSerializer, \
+from .serializers import RestaurantSerializer, \
     ReservationSerializer
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.views.generic.edit import FormView
 from django.views.generic.edit import FormMixin
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from .forms import RegisterForm
 
@@ -21,8 +22,6 @@ def index(request):
     }
 
     return render(request, 'app/index.html', data)
-
-
 
 
 def register(request):
@@ -99,35 +98,12 @@ def list_reservations(request, id):
     return render(request, 'app/reservation/list.html', data)
 
 
-class RestaurantDetailView(FormMixin, DetailView):
+class RestaurantDetailView(DetailView):
+
     template_name = 'app/restaurant/detail.html'
     queryset = Restaurant.objects.all()
-    def get_object(self):
-        slug = self.kwargs.get('slug')
-        return get_object_or_404(Restaurant, slug=slug)
-    model = Restaurant
-    form_class = ReservationForm
 
-    def get_success_url(self):
-        return reverse('index')
 
-    def get_context_data(self, **kwargs):
-        context = super(RestaurantDetailView, self).get_context_data(**kwargs)
-        context['form'] = self.get_form()
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        new_comment = form.save(commit=False)
-        new_comment.post = self.get_object()
-        return super(RestaurantDetailView, self).form_valid(form)
 
 
 def create_reservation(request, slug):
